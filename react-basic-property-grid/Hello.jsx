d0cc996ebb220d66c62e424091306ca310a0bb73
Chris Draper <chrisd9809@gmail.com>
2016-07-20 17:46:06 -0400

    Re-organize all of the tutorials I completed into a single repository for easy access

---
+++ react-basic-property-grid/Hello.jsx
@@ -0,0 +1,18 @@
+/** @jsx React.DOM */
+
+'use strict'
+
+var React = require('react')
+
+var Hello = React.createClass({
+
+    displayName: 'HelloReact',
+
+    render: function() {
+        return (
+            <div>Hello React!</div>
+        );
+    }
+});
+
+module.exports = Hello;
